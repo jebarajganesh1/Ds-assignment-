ac38a387ffdedb30b17de0a5b295727f19ca4678
jebarajganesh1 <60725919+jebarajganesh1@users.noreply.github.com>
2020-10-16 15:53:31 +0530

    Create Jeba.py

---
+++ Jeba.py
@@ -0,0 +1,39 @@
+def bracketmatching(expr): 
+    stack = [] 
+  
+    
+    for char in expr: 
+        if char in ["(", "{", "["]: 
+  
+             
+            stack.append(char) 
+        else: 
+   
+            if not stack: 
+                return False
+            current_char = stack.pop() 
+            if current_char == '(': 
+                if char != ")": 
+                    return False
+            if current_char == '{': 
+                if char != "}": 
+                    return False
+            if current_char == '[': 
+                if char != "]": 
+                    return False
+  
+    
+    if stack: 
+        return False
+    return True
+  
+  
+  
+if __name__ == "__main__": 
+    expr = "{()}[]"
+      
+    
+    if bracketmatching(expr): 
+        print("Balanced") 
+    else: 
+        print("Not Balanced") 
